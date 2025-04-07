5fa6f59c2a25c11b5c262dcfec75e71cd8bd8dc7
Adiyh <adarshyh354@gmail.com>
2025-04-07 11:36:14 +0530

    Monday morning

--- HelloWorld/HelloWorld/Class1.cs
+++ HelloWorld/HelloWorld/Class1.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloWorld
-{
-    class Class1
-    {
-        static void Main(String[] args)
-        {
-            Console.Write("Adsrsff");
-        }
-    }
-}
+﻿
--- HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30e5125641949414eca5716392eb03b12dff4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af95444b6c2fc8fb5842751d44096f4057593f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
