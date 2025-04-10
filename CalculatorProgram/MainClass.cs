6653113a9c481a38d7c9da5c04334f1676c383e4
Adiyh <adarshyh354@gmail.com>
2025-04-10 10:56:22 +0530

    Message

---
+++ CalculatorProgram/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorProgram.models;
+
+namespace CalculatorProgram
+{
+    internal class MainClass
+
+    {
+        public static void Main()
+        {
+            Menu.menu();
+            Interest.InterstLogic();
+            Arithmitic.ArithmiticLogic();
+
+
+
+        }
+    }
+}
---
+++ CalculatorProgram/models/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorProgram.Operations;
+
+namespace CalculatorProgram.models
+{
+    internal class Menu
+    {
+        public static int choice;
+
+        public static void menu()
+        {
+           
+                try
+                {
+                    Console.WriteLine("Choose the operation you want to perform");
+                    Console.WriteLine("1.Arithmatic Operations");
+                    Console.WriteLine("2.Simple interst");
+
+                    choice = int.Parse(Console.ReadLine());
+                }
+               
+                
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Enter number in 1 or 2");
+
+                }
+            
+
+            switch (choice)
+            {
+                case 1: FindArithmatic.find(); 
+                    break;
+                case 2: FindInterst.findinterst();
+                    break;
+              default:  Console.WriteLine("Invalid choice, please try again.");
+                    break;  
+
+            }
+        }
+    }
+}
