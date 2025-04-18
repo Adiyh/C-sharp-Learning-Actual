7f2902ffd8cb5de64d0ef24961178a4f7b7a1262
Adiyh <adarshyh354@gmail.com>
2025-04-18 15:53:24 +0530

    Commit

---
+++ CollectioProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class ADARSH
+{
+    public void dboss()
+    {
+        int a = 12, b = 8;
+        int c=0;
+        
+
+
+        Console.WriteLine(c);
+    }
+        public static void Main(string[] args)
+    {
+        ADARSH adi = new ADARSH();
+        adi.dboss();
+    }
+}
+    
+
---
+++ interfaceapp/Interface1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceapp
+{
+    public interface Interface1
+    {
+        void Add(int a, int b);
+    }
+    public interface Interface2
+    {
+        void Sub(int a, int b);
+    }
+   
+    public class InterfaceClass : Interface1, Interface2
+    {
+         public void Add(int a, int b) {
+            Console.WriteLine(a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+
+        }
+    }
+}
