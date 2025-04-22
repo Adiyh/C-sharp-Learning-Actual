68d1056d8e9f5e619e264cef2438d471c6d1f75c
Adiyh <adarshyh354@gmail.com>
2025-04-22 18:15:03 +0530

    message

---
+++ DictionaryApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, string> friction = new Dictionary<string, string>();
+        friction.Add("aditya", "A");
+        friction.Add("Akash","B");
+        friction["Anup"] ="C";
+
+        foreach (string key in friction.Keys)
+        {
+            Console.WriteLine("Name:"+ key);
+        }
+        if (friction.ContainsKey("Anup"))
+        {
+            Console.WriteLine("Anup's name is :" + friction["Anup"]);
+        }
+    }
+}
